7d56877bacab7ff384f55efa3c5d4dc37c1d786c
Yashdeep Hinge <137262766+yhinge@users.noreply.github.com>
2026-07-04 22:13:02 +0530

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+        for (let i = 0; i < strs.length; i++) {
+            let word = strs[i];
+            let key = word.split("").sort().join("");
+            if (map[key]) {
+                map[key].push(word);
+            } else {
+                map[key] = [word];
+            }
+        }
+        const result = [];
+        for (const k in map) {
+            result.push(map[k]);
+        }
+        return result;
+    }
+}
